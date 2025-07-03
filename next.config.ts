9efe7c813e036f51245acca33a976b6f10b56bac
Adam Love <iwilldevelopcodeforyou@gmail.com>
2025-07-03 14:51:30 -0500

    updated nextConfig with output: unneru

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     basePath: '/unnuo',
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
     images: {
     unoptimized: true, // Optional: Use this if you're hosting images statically
   },
