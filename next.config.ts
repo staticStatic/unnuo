5327284492d193d4bf09fc644550ad6abf6276ca
Adam Love <iwilldevelopcodeforyou@gmail.com>
2025-07-05 17:56:52 +0000

    moved isDev asset prefix; unnuoni

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = !!process.env.CODESPACE_NAME;
+const isDev = !!process.env.CODESPACE_NAME;
 
 const nextConfig: NextConfig = {
-    basePath: isProd ? '/unnuo' : '',
-//    assetPrefix: isProd ? '/unnuo' : "",
+    //basePath: isDev ? '' : '/unnuo',
+    assetPrefix: isDev ? '' : '/unnuo',
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: {
